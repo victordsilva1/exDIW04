0b0ba6ab3f85577f6592cd3009308a46fe2070a8
Victor da Silva Folgado <1526506@sga.pucminas.br>
2024-05-03 20:26:52 -0300

    Ex 01 lista DIW

---
+++ funcionalidade.js
@@ -0,0 +1,8 @@
+function idade() {
+    var yearbirth = prompt("Qual ano voce nasceu: ")
+    var age = 2024 - yearbirth
+    alert(`Voce tem ${age}`)
+    var age2050 = 2050 - yearbirth
+
+    alert(`Em 2050 voce tera ${age2050}`)
+}
